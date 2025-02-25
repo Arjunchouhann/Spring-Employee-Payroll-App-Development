16a8cc338d372809ddeae1028abdd97cc186486d
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-25 15:20:31 +0530

    [Arjun] Add.Introducing Logger and DTO in Employeepayroll app

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeeEntity.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeeEntity.java
@@ -1,22 +1,18 @@
-package com.bridgelabz.employeepayrollapp.model;
+package com.example.payroll.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
 public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private double salary;
     private String department;
+    private Double salary;
 }
