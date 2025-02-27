f42f542aa89eb597d5a915e8c03f60c0fb5e680e
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-27 14:03:35 +0530

    [Arjun] Add.Database setting as Environment Variable

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -13,3 +13,4 @@ public class EmployeePayrollAppApplication {
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
 	}
 }
+
--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
@@ -26,14 +26,18 @@ public class EmployeeController {
     // To get all the employees
     @GetMapping
     public List<EmployeeEntity> getAllEmployees() {
-        logger.info("All employee endpoint called");
+        if (logger.isInfoEnabled()) {
+            logger.info("All employee endpoint called");
+        }
         return employeeService.getAllEmployee();
     }
 
     // To get employee by id
     @GetMapping("/get/{id}")
     public ResponseEntity<Optional<EmployeeEntity>> getEmployeeById(@PathVariable Long id) {
-        logger.info("By ID employee endpoint called with ID: {}", id);
+        if (logger.isInfoEnabled()) {
+            logger.info("By ID employee endpoint called with ID: {}", id);
+        }
         Optional<EmployeeEntity> employee = employeeService.getEmployeeById(id);
         if (employee.isPresent()) {
             return ResponseEntity.ok(employee);
@@ -47,14 +51,18 @@ public class EmployeeController {
     // To create a new employee
     @PostMapping("/create")
     public EmployeeEntity addEmployee(@RequestBody EmployeeDTO employeeDTO) {
-        logger.info("Create employee endpoint called");
+        if (logger.isInfoEnabled()) {
+            logger.info("Create employee endpoint called");
+        }
         return employeeService.addEmployee(employeeDTO);
     }
 
     // To update the employee
     @PutMapping("/update/{id}")
     public ResponseEntity<EmployeeEntity> updateEmployee(@PathVariable Long id, @RequestBody EmployeeDTO employeeDTO) {
-        logger.info("Update employee endpoint called with ID: {}", id);
+        if (logger.isInfoEnabled()) {
+            logger.info("Update employee endpoint called with ID: {}", id);
+        }
         EmployeeEntity updatedEmployee = employeeService.updateEmployee(id, employeeDTO);
         if (updatedEmployee != null) {
             return ResponseEntity.ok(updatedEmployee);
@@ -67,7 +75,9 @@ public class EmployeeController {
     // To delete the employee
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
-        logger.info("Delete employee endpoint called with ID: {}", id);
+        if (logger.isInfoEnabled()) {
+            logger.info("Delete employee endpoint called with ID: {}", id);
+        }
         boolean deleted = employeeService.deleteEmployee(id);
         if (deleted) {
             return ResponseEntity.noContent().build();
